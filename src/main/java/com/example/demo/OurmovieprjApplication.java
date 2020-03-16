4c625fc902cff90305675959618aac0cca0f28cf
yuanman-994 <592777495@qq.com>
2020-03-16 16:30:00 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/OurmovieprjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurmovieprjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( OurmovieprjApplication.class, args );
+    }
+
+}

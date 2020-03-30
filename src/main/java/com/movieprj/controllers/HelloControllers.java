d9ebbfc39f2ab617eb42f1db69b4e50f57bf2f25
cldsk <1941088377@qq.com>
2020-03-30 16:05:08 +0800

    团购页面调整，加入排序，分页

--- src/main/java/com/movieprj/controllers/HelloControllers.java
+++ src/main/java/com/movieprj/controllers/HelloControllers.java
@@ -1,6 +1,5 @@
 package com.movieprj.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;

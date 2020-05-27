d61cfe675298fdecda259b17dfdfe1d18d78a656
cldsk <1941088377@qq.com>
2020-05-27 11:50:26 +0800

    完成搜索

--- src/main/java/com/movieprj/controllers/SearchController.java
+++ src/main/java/com/movieprj/controllers/SearchController.java
@@ -1,6 +1,8 @@
 package com.movieprj.controllers;
 
+import com.movieprj.beans.Movies;
 import com.movieprj.services.ArticleService;
+import com.movieprj.services.MoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,12 +10,16 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class SearchController {
     @Autowired
     private ArticleService articleService;
+
+    @Autowired
+    private MoviesService moviesService;
 
     @RequestMapping ("/search")
     public String  search(){
@@ -29,5 +35,12 @@ public class SearchController {
         int offset = pageSize*(aimPage-1);
         int limit = offset+pageSize;
         return articleService.getArticleWithLimit(limit,offset,searchText);
+    }
+
+    @GetMapping("/search/searchMovie")
+    @ResponseBody
+    public Map<String,Object> searchMovie(@RequestParam("searchText") String searchText){
+        Map<String, Object> map = moviesService.searchMovies(searchText,"datenone","statusnone");
+        return map;
     }
 }
